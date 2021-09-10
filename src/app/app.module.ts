d6a1f9dae5c3d96e93eb4b6a683e282e8fa8fcc7
Priscila Ravanelli <priscila.andreani@b2wdigital.com>
2021-09-10 16:37:00 -0300

    add footer, main and top component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
