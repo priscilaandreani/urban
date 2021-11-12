16e5e378812aa740bdfe933c0bcf11e6f7dc56f1
priscila andreani <priscilaandreani@gmail.com>
2021-11-12 16:43:38 -0300

    feat: add fun and restaurants components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,13 @@ import { AppComponent } from './app.component';
 import { TopComponent } from './top/top.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { DiversaoComponent } from './diversao/diversao.component';
+import { FunComponent } from './fun/fun.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 @NgModule({
-  declarations: [AppComponent, TopComponent, MainComponent, FooterComponent],
+  declarations: [AppComponent, TopComponent, MainComponent, FooterComponent, RestaurantesComponent, DiversaoComponent, FunComponent, RestaurantsComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
