2bab155bce87ec85a3361ef0f130637052c43c83
priscila andreani <priscilaandreani@gmail.com>
2022-01-06 17:07:47 -0300

    feat: purchase-order: add two way databiding method to forms

--- src/app/purchase-order/purchase-order.component.ts
+++ src/app/purchase-order/purchase-order.component.ts
@@ -3,13 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-purchase-order',
   templateUrl: './purchase-order.component.html',
-  styleUrls: ['./purchase-order.component.css']
+  styleUrls: ['./purchase-order.component.css'],
 })
 export class PurchaseOrderComponent implements OnInit {
+  public address: string = '';
+  public complement: string = '';
+  public number: string = '';
+  public paymentMethod: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  public updateAddress(address: string): void {
+    this.address = address;
+    console.log(this.address);
   }
 
+  public updateNumber(number: string): void {
+    this.number = number;
+    console.log(this.number);
+  }
+
+  public updateComplement(complement: string): void {
+    this.complement = complement;
+    console.log(this.complement);
+  }
+
+  public updatePaymentMethod(method: string): void {
+    this.paymentMethod = method;
+    console.log(this.paymentMethod);
+  }
+
+  ngOnInit(): void {}
 }
